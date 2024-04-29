2066b9e1e1894763e3edf218666fe65a0a64443a
Michał Urawski <michurawski@gmail.com>
2024-04-29 11:49:29 +0200

    Scoreboard presenter implementation

--- src/model/presenter-config.ts
+++ src/model/presenter-config.ts
@@ -1,5 +1,7 @@
 import { ScoreboardData } from "./scoreboard-data";
 
 export interface PresenterConfig {
-    sortingFunction?: (match1: ScoreboardData, match2: ScoreboardData) => number;
+    sortingFunction?: ScoreboardSortingStrategy
 }
+
+export type ScoreboardSortingStrategy = (match1: ScoreboardData, match2: ScoreboardData) => number;
--- src/scoreboard-presenter.ts
+++ src/scoreboard-presenter.ts
@@ -2,13 +2,27 @@ import { IScoreboardManager } from "./i-scoreboard-manager";
 import { IScoreboardPresenter } from "./i-scoreboard-presenter";
 import { Match } from "./model/match";
 import { PresenterConfig } from "./model/presenter-config";
+import { ScoreboardData } from "./model/scoreboard-data";
 
 export class ScoreboardPresenter implements IScoreboardPresenter {
+
+    private defaultConfig: Required<PresenterConfig> = {
+        sortingFunction: (match1: ScoreboardData, match2: ScoreboardData) => {
+            const totalScore = (scoreBoardData: ScoreboardData) => scoreBoardData.match.homeTeam.score + scoreBoardData.match.awayTeam.score;
+            const totalScoreOrder = totalScore(match2) - totalScore(match1);
+            if (totalScoreOrder > 0) {
+                return totalScoreOrder;
+            }
+
+            return match2.matchOrder - match1.matchOrder;
+        }
+    }
 
     constructor(private scoreboardManager: IScoreboardManager, private presenterConfig: PresenterConfig = {}) {}
 
     getSummary(): Array<Match> {
-        return [];
+        const data = [...this.scoreboardManager.getData()].sort((match1, match2) => this.defaultConfig.sortingFunction(match1, match2));
+        return data.map(data => data.match);
     }
 
 }
